df44e8ec3321ad29c986615aa1c0f8899b839ba2
Cristian Pisco <cristian.pisco@jelou.ai>
2024-10-17 09:54:47 -0500

    changes in .env files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+console.log(`The connection URL is ${process.env.DATABASE_URL}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
